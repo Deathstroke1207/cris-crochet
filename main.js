eae933209a5892fb449d62b8e696f46cf2710b41
Facundo Espeche <deaddestrouk@gmail.com>
2023-09-29 00:17:58 -0300

    agregando adaptabilidad de pantallas

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ document.querySelector(".button-list").addEventListener("click", () => {
   document.querySelector(".productos").classList.toggle("show");
 });
 
+document.querySelector(".inicio-btn").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("mostrar");
+  });
+
 ScrollReveal().reveal('.card', { delay: 500 });
